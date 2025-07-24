b8e96af5590f98e7a13c6967435fc6667496d898
ShreyaVispute021 <shreyagvispute@gmail.com>
2025-07-24 23:57:36 +0530

    Update App.jsx

--- react-setup/App.jsx
+++ react-setup/App.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AutoBrandForm from "./AutoBrandForm";
 import Entry from "./entry";
 
 function App() {
   return (
     <>
-      <Entry />
-      <AutoBrandForm />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Entry />} />
+          <Route path='/autobrandform' element={<AutoBrandForm />} />
+        </Routes>
+      </Router>
 
     </>
   );
